af77b399e55aa6789f60e5eef29c963f06810941
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2016-04-24 18:13:53 +0200

    Expanded tests

--- test/namenums/core_test.clj
+++ test/namenums/core_test.clj
@@ -2,14 +2,25 @@
   (:require [namenums.core :refer :all]
             [midje.sweet :refer :all]))
 
-(fact
- "A number outside of allowed range will be rejected"
- (name-it -1) => nil
- (name-it 1000) => nil)
+(tabular
+ (fact
+  "A number outside of allowed range will be rejected"
+  (name-it ?n) => nil)
+ ?n
+ -1
+ 1000
+ Integer/MAX_VALUE)
 
-(fact
- "Numbers are named correctly"
- (name-it 0) => "zero"
- (name-it 1) => "one"
- (name-it 24) => "twenty-four"
- (name-it 999) => "nine hundred and ninety-nine")
+(tabular
+ (fact
+  "Numbers are named correctly"
+  (name-it ?num) => ?text)
+ ?num ?text
+ 0 "zero"
+ 1 "one"
+ 24 "twenty-four"
+ 200 "two hundred"
+ 240 "two hundred and fourty"
+ 243 "two hundred and fourty-three"
+ 309 "three hundred and nine"
+ 999 "nine hundred and ninety-nine")
